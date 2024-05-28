7d55bca9a5138333294c2f7d52aa54cf0601e525
GABRIEL AURELIANO OLIVEIRA <2006572@unimar.br>
2024-05-28 00:54:27 -0300

    Reapply "Novos Codigos 28/05"
    This reverts commit afae59fa44f96b4c4fa6008b7f01876f32fb753a.

--- Main/ConsoleApp1/Program.cs
+++ Main/ConsoleApp1/Program.cs
@@ -8,30 +8,64 @@ namespace ConsoleApp1
 {
     public class Program
     {
+    static List<Produtos> listaDeProdutos = new List<Produtos>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("------------ Cadastro De Produtos ------------");
-            string Produtos = Console.ReadLine();
-            switch (Produtos)
+            while (true)
             {
-                case "1":
-                    Console.WriteLine(" 1 - Cadastrar Nome: ");
-                    break;
-                case "2":
-                    Console.WriteLine(" 2 - Cadastrar Preços: ");
-                break;
-                case "3":
-                    Console.WriteLine(" 3 - Cadastrar Estoque: ");
-                break;
-                    case "4":
-                    Console.WriteLine(" 4 - Cadastrar Código Do Produto");
-                    break;
-                case "5":
-                    Console.WriteLine("Digite Sair Para Sair Do Sistema!");
-                    break;
+                Console.WriteLine("------------ Cadastro De Produtos ------------");
+                Console.WriteLine(" 1 - Digite Para Cadastrar Produto: ");
+                Console.WriteLine(" 2 - Digite Para Acessar Produtos: ");
+                Console.WriteLine(" 3 - Digite Para Sair Do Sistema: ");
+
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        CadastrarProduto();
+                        break;
+                    case "2":
+                        MostrarProdutos();
+                        break;
+                    case "3":
+                        Console.WriteLine("Saindo do sistema...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
             }
+        }
 
+        static void CadastrarProduto()
+        {
+            Produtos novoProduto = new Produtos();
+            Console.Write("Nome do Produto: ");
+            novoProduto.Nome = Console.ReadLine();
+            Console.Write("Preço do Produto: ");
+            novoProduto.Preco = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Estoque do Produto: ");
+            novoProduto.Estoque = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Código do Produto: ");
+            novoProduto.Id = Convert.ToInt32(Console.ReadLine());
 
+            listaDeProdutos.Add(novoProduto);
+            Console.WriteLine("Produto cadastrado com sucesso!\n");
+        }
+
+        static void MostrarProdutos()
+        {
+            Console.WriteLine("Lista de Produtos:");
+            foreach (var produto in listaDeProdutos)
+            {
+                Console.WriteLine($"Nome: {produto.Nome}," +
+                $" Preço: {produto.Preco:F2}, Estoque: {produto.Estoque}, Código: {produto.Id}");
+            }
+            Console.WriteLine("Pressione Enter Para Continuar");
+            Console.ReadLine();
         }
     }
 }
